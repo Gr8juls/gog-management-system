11f59071928c2513a9f60982015bc18dfa72031b
Julius <rubajul@gmail.com>
2026-09-15 21:13:48 +0200

    commit

--- start-mongodb.bat
+++ start-mongodb.bat
@@ -6,6 +6,23 @@ set MONGO_DIR=%USERPROFILE%\mongodb-local\bin
 set DATA_DIR=%USERPROFILE%\mongodb-local\data\db
 set LOG_DIR=%USERPROFILE%\mongodb-local\log
 
+if not exist "%LOG_DIR%" mkdir "%LOG_DIR%"
+if not exist "%DATA_DIR%" mkdir "%DATA_DIR%"
+
+:: Check if mongod is already running
+tasklist /FI "IMAGENAME eq mongod.exe" 2>NUL | find /I /N "mongod.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    echo [GOG] MongoDB is already running on this machine.
+    pause
+    exit /b 0
+)
+
+:: If not running, clean up any stale lock file from previous abnormal termination
+if exist "%DATA_DIR%\mongod.lock" (
+    echo [GOG] Removing stale lock file...
+    del /f /q "%DATA_DIR%\mongod.lock"
+)
+
 echo [GOG] Starting local MongoDB replica set on port 27017...
 "%MONGO_DIR%\mongod.exe" ^
   --dbpath "%DATA_DIR%" ^
